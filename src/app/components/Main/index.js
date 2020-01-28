39dd00019ef70564cdff339f7ffb18b7c8000ac0
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2020-01-28 17:45:43 +0200

    Add main component

---
+++ src/app/components/Main/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./index.css";
+
+function Main() {
+  return (
+    <main className="Main">
+      <div className="ProductsList">
+        <ul>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+          <li className="ProductsList__item">
+            <h3>Microvawe</h3>
+            <p>Very Nice Short Description</p>
+            <div>
+              <p>$30</p>
+              <button>Add to Cart</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
--- src/app/index.js
+++ src/app/index.js
@@ -2,66 +2,14 @@ import React from "react";
 import "./index.css";
 
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="Main">
-        <div className="ProductsList">
-          <ul>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-            <li className="ProductsList__item">
-              <h3>Microvawe</h3>
-              <p>Very Nice Short Description</p>
-              <div>
-                <p>$30</p>
-                <button>Add to Cart</button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </main>
+      <Main />
       <Footer />
     </div>
   );
